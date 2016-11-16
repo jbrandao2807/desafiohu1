b4b957c723e8aae05cbf1ea9394a5e99a1895da4
Jonatan Brandao <raziel2807@gmail.com>
2016-11-15 22:08:29 -0200

    [Improve] - Try catch

--- services/availability.js
+++ services/availability.js
@@ -10,85 +10,89 @@ function daydiff(first, second) {
 }
 
 
-
 module.exports = function (formData) {
 
-    var result = null;
-    var cachedResult = caching.resultsCache.get(formData.toString());
 
-    if (cachedResult) {
-        return cachedResult;
-    }
+    try {
+        var result = null;
+        var cachedResult = caching.resultsCache.get(formData.toString());
 
-    var locationNames = caching.fileCache.get('location-names');
-    var hotelsList = caching.fileCache.get('hotels');
-    var availability = caching.fileCache.get('availability');
+        if (cachedResult) {
+            return cachedResult;
+        }
+
+        var locationNames = caching.fileCache.get('location-names');
+        var hotelsList = caching.fileCache.get('hotels');
+        var availability = caching.fileCache.get('availability');
 
 
-    if (formData.location) {
+        if (formData.location) {
 
-        var hotelIds = locationNames[formData.location.toString().trim()];
+            var hotelIds = locationNames[formData.location.toString().trim()];
 
-        if (hotelIds) {
-            var hotelsList = hotelsList.map(function (element, index, currentArray) {
-                var isValid = false;
-                for (var i = 0, l = hotelIds.length; i < l; i++) {
-                    var hotelId = hotelIds[i];
-                    if (element.id === hotelId) {
-                        isValid = true;
+            if (hotelIds) {
+                var hotelsList = hotelsList.map(function (element, index, currentArray) {
+                    var isValid = false;
+                    for (var i = 0, l = hotelIds.length; i < l; i++) {
+                        var hotelId = hotelIds[i];
+                        if (element.id === hotelId) {
+                            isValid = true;
+                        }
                     }
-                }
-                if (isValid) {
-                    return element;
-                } else {
-                    return null;
-                }
-            });
+                    if (isValid) {
+                        return element;
+                    } else {
+                        return null;
+                    }
+                });
 
-            hotelsList = unique(hotelsList);
-
+                hotelsList = unique(hotelsList);
 
 
-            result = [];
-            for (var i = 0, l = availability.length; i < l; i++) {
-                var avItem = availability[i];
+                result = [];
+                for (var i = 0, l = availability.length; i < l; i++) {
+                    var avItem = availability[i];
 
-                hotelsList.map(function (element) {
-                    if (element && avItem[0].toString() === element.id) {
-                        var valid = true;
+                    hotelsList.map(function (element) {
+                        if (element && avItem[0].toString() === element.id) {
+                            var valid = true;
 
-                        if (formData.begin || formData.finish) {
-                            var avItemDate = Date.parse(avItem[1]);
-                            if (!(formData.begin && Date.parse(formData.begin) <= avItemDate)) {
-                                valid = false;
+                            if (formData.begin || formData.finish) {
+                                var avItemDate = Date.parse(avItem[1]);
+                                if (!(formData.begin && Date.parse(formData.begin) <= avItemDate)) {
+                                    valid = false;
+                                }
+                                if (!(formData.finish && Date.parse(formData.finish) >= avItemDate)) {
+                                    valid = false;
+                                }
+                                if ((!(parseInt(avItem[3]) > parseInt(daydiff(Date.parse(formData.begin), Date.parse(formData.finish)))))) {
+                                    valid = false;
+                                }
+
                             }
-                            if (!(formData.finish && Date.parse(formData.finish) >= avItemDate)) {
-                                valid = false;
-                            }
-                            if ((!(parseInt(avItem[3]) > parseInt(daydiff(Date.parse(formData.begin), Date.parse(formData.finish)))))) {
-                                valid = false;
+                            if (valid) {
+                                result.push({
+                                    id: avItem[0],
+                                    date: avItem[1],
+                                    minimumNights: avItem[3],
+                                    location: element.location,
+                                    name: element.name
+                                })
                             }
 
                         }
-                        if (valid) {
-                            result.push({
-                                id: avItem[0],
-                                date: avItem[1],
-                                minimumNights: avItem[3],
-                                location: element.location,
-                                name: element.name
-                            })
-                        }
-
-                    }
-                })
+                    })
+                }
             }
         }
+
+
+        caching.resultsCache.set(formData.toString(), result);
+        return result;
+    } catch (e) {
+        console.log(e);
+        return null;
     }
-
-
-    caching.resultsCache.set(formData.toString(), result);
-    return result
 
 };
 
